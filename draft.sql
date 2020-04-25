92e53598ba3ba2ec8e16e7f6ab2f55435410fdda
Ryan Ashcraft <rashcraft1991@gmail.com>
2020-04-25 12:41:41 -0500

    SQL update

--- draft.sql
+++ draft.sql
@@ -1,11 +1,13 @@
+drop table if exists drafts;
+
 CREATE TABLE drafts (
     id serial,
-    round int not null,
-    round_pick int not null,
-    overall_pick int not null,
-    player_name varchar(50) not null,
-    team VARCHAR(30) not null,
-    position varchar(5) not null,
-    college varchar(50) not null,
-    year int not null
+    "Round" int not null,
+    "Round_Pick" int not null,
+    "Overall_Pick" int not null,
+    "Name" varchar(50) not null,
+    "Team" VARCHAR(30) not null,
+    "Position" varchar(5) not null,
+    "College" varchar(50),
+    "Year" int not null
 );
