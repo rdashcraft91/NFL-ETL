3a875f4e513dd7521f834a3be8c8213eaa199684
Luis <luis.olguin89@gmail.com>
2020-04-25 19:41:37 -0500

    added sql queries to create views

--- Luis Olguin/salaries_schema.sql
+++ Luis Olguin/salaries_schema.sql
@@ -1,13 +1,13 @@
-DROP table salaries
+--DROP table salaries
 
 CREATE TABLE salaries(
-	"Salary_Rank" INT NOT NULL,
-	"uid" VARCHAR(30) PRIMARY KEY,
-	"Player" VARCHAR(30) NOT NULL,
-	"First_Name" VARCHAR(30) NOT NULL,
-	"Last_Name" VARCHAR(30) NOT NULL,
-	"Player_Position" VARCHAR(30) NOT NULL,
-	"Abrv" VARCHAR (30) NOT NULL,
-	"Team" VARCHAR (50),
-	"Salary" INT	
+	salary_rank INT NOT NULL,
+	uid VARCHAR(30) PRIMARY KEY,
+	player VARCHAR(30) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	player_position VARCHAR(30) NOT NULL,
+	abrv VARCHAR (30) NOT NULL,
+	team VARCHAR (50),
+	salary INT	
 )
